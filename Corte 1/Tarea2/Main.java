3a6045d2613c7d0631dc2c586ef6c578434211b8
Sandoval015 <ynbolanoss@udistrital.edu.co>
2025-04-10 16:26:23 -0500

    Update Main.java

--- Corte 1/Tarea2/Main.java
+++ Corte 1/Tarea2/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
         Operations resultado = new Operations();
 
-        resultado.sumar(3, 2); // Realizar la suma
+        resultado.sumar(3, 2); //  la suma
         System.out.println(resultado.getResultado()); // Obtener el resultado usando getResultado()
 
-        resultado.restar(10, 5); // Realizar la resta
+        resultado.restar(10, 5); //  la resta
         System.out.println(resultado.getResultado());
 
-        resultado.multiplicar(2, 2); // Realizar la multiplicación
+        resultado.multiplicar(2, 2); // la multiplicacion
         System.out.println(resultado.getResultado());
 
-        //resultado.potencia(2, 4); // Realizar la potencia
+        //resultado.potencia(2, 4); //  la potencia
         //System.out.println(resultado.getResultado());
     }
 }
